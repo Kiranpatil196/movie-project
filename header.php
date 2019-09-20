2d0fbc3c3725e04d647b247d84765e401e64b94c
pp_mb <abcd>
2019-09-20 17:41:07 +0530

    second commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<?php
+<div class="div1">
+<h1 class="first" align="center" style="background-color:#6666FF">NETFLIX</h1>
+</div>
+<div class="div3">
+
+<h3 align="center"><a href="main.html">HOME</a>
+<a href="register.html">SIGN UP</a>
+<a href="login.html">SIGN IN</a>
+<a href="contactus.html">CONTACT US</a>
+</h3>
+</div>
+
+<marquee direction="right"><h3><i><b>*Latest Movies*</b></i></h3></marquee>;
+?>
---
+++ main.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Movies</title>
+<link rel="stylesheet" href="css/main.css"/>
+</head>
+
+<body>
+<section>
+
+  <nav>
+  <center>
+    <ul>
+      <li><a href="new.html"><b>NEW MOVIES</b></a></li>
+	  <br/>
+      <li><a href="old.html"><b>OLD MOVIES</b></a></li>
+	   <br/>
+      <li><a href="popular.html"><b>POPULAR MOVIES</b></a></li>
+    </ul>
+	</center>
+  </nav>
+  <article>
+<a href="#"><img src="images/kabirsingh.jpg" width="320" height="300" /></a>&nbsp;&nbsp;
+<a href="#"><img src="images/bharat.jpg" width="320" height="300" /></a>&nbsp;&nbsp;
+<a href="#"><img src="images/bahubali.jpg" width="320" height="300" /></a>&nbsp;&nbsp;
+<br />
+<a href="#"><img src="images/loveyatri.jpg" width="320" height="300" /></a>&nbsp;&nbsp;
+<a href="#"><img src="images/race3.jpg" width="320" height="300" /></a>&nbsp;&nbsp;
+<a href="#"><img src="images/raees.jpg" width="320" height="300" /></a>&nbsp;&nbsp;
+  </article>
+ </section>
+
+  <h3 class="footer" align="right"> Ph: (08338) 221391 , 9964639584 &nbsp;&nbsp;&nbsp;&nbsp; Email: netflix@gmail.com &nbsp;</h3>
+ <h3>
+ </body>
+ </html>
